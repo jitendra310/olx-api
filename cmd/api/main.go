fcd4c261dbc899e9116d49b7ebc2d995ae5a62fe
Jitendra Vishwakarma <jitendravishwakarma310@gmail.com>
2026-08-04 01:00:53 +0530

    add server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
+)
 
 func main() {
-	fmt.Println("Welcome to olx-api..")
+	//make own router
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		w.Write([]byte(`{"status":"ok"}`))
+	})
+
+	srv := http.Server{
+		Addr:         ":8090",
+		Handler:      mux,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 30,
+		IdleTimeout:  time.Second * 60,
+	}
+
+	// err := http.ListenAndServe(":8090", mux) //we can use this also for temp
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("server faild: %v", err)
+	}
 }
