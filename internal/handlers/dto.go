6543f90b3f70c930a7541a30c211a5a07319093d
Jitendra Vishwakarma <jitendravishwakarma310@gmail.com>
2026-08-13 21:06:32 +0530

    add listings validation

--- internal/handlers/dto.go
+++ internal/handlers/dto.go
@@ -32,5 +32,21 @@ func (req CreateListingsRequest) Validate() error {
 	if strings.TrimSpace(req.Title) == "" {
 		return &ValidationError{Field: "title", msg: "must not be empty"}
 	}
+	if len(req.Title) > 200 {
+		return &ValidationError{Field: "title", msg: "must be at most 200 charaters"}
+	}
+	if strings.TrimSpace(req.Description) == "" {
+		return &ValidationError{Field: "description", msg: "must not be empty"}
+	}
+	if len(req.Description) > 5000 {
+		return &ValidationError{Field: "description", msg: "must be at most 5000 charaters"}
+	}
+	if req.Price <= 0 {
+		return &ValidationError{Field: "price", msg: "must be grater then zero"}
+	}
+	if strings.TrimSpace(req.City) == "" {
+		return &ValidationError{Field: "city", msg: "must not be empty"}
+	}
+
 	return nil
 }
